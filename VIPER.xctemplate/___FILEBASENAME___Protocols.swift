9ab8a0bb475bb4c48d6feaf518ffbeebb5e13cba
İsmail Onur KOZPINAR <kozpinar.onur@gmail.com>
2022-02-10 13:31:03 +0300

    use AnyObject instead of class

--- VIPER.xctemplate/___FILEBASENAME___Protocols.swift
+++ VIPER.xctemplate/___FILEBASENAME___Protocols.swift
@@ -10,25 +10,25 @@
 
 import UIKit
 
-protocol ___VARIABLE_productName:identifier___PresenterToViewProtocol: class {
+protocol ___VARIABLE_productName:identifier___PresenterToViewProtocol: AnyObject {
     
 }
 
-protocol ___VARIABLE_productName:identifier___InteractorToPresenterProtocol: class {
+protocol ___VARIABLE_productName:identifier___InteractorToPresenterProtocol: AnyObject {
     
 }
 
-protocol ___VARIABLE_productName:identifier___PresenterToInteractorProtocol: class {
+protocol ___VARIABLE_productName:identifier___PresenterToInteractorProtocol: AnyObject {
     var presenter: ___VARIABLE_productName:identifier___InteractorToPresenterProtocol? {get set}
     
     
 }
 
-protocol ___VARIABLE_productName:identifier___PresenterToRouterProtocol: class {
+protocol ___VARIABLE_productName:identifier___PresenterToRouterProtocol: AnyObject {
     
 }
 
-protocol ___VARIABLE_productName:identifier___ViewToPresenterProtocol: class {
+protocol ___VARIABLE_productName:identifier___ViewToPresenterProtocol: AnyObject {
     var view: ___VARIABLE_productName:identifier___PresenterToViewProtocol? {get set}
     var interactor: ___VARIABLE_productName:identifier___PresenterToInteractorProtocol? {get set}
     var router: ___VARIABLE_productName:identifier___PresenterToRouterProtocol? {get set}
